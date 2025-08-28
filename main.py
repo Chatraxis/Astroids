be413bdac859ec95ed51208f2aa491f049f92e58
Chatraxis <lammer.christoph@gmx.at>
2025-08-28 17:54:43 +0200

    added frame counting to limit the FPS

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     number = 1
+    time = pygame.time.Clock()
+    dt = 0
     while number != 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,7 +18,7 @@ def main():
         pygame.Surface.fill(screen, color="black")
 
         pygame.display.flip()
-
-
+        dt = time.tick(60)/1000
+        
 if __name__ == "__main__":
     main()
